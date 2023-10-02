bbb067847ecae33ab23aecb868631a148e1eb158
Margo1212 <sirachenko944@gmail.com>
2023-10-02 17:02:54 +0200

    add navigation

--- frontend/src/lib/components/Navigation/Navigation.tsx
+++ frontend/src/lib/components/Navigation/Navigation.tsx
@@ -2,14 +2,24 @@ import Link from "next/link";
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
+    <nav className="w-full flex justify-between">
+      <div>Artladyclinic</div>
+      <ul className="px-24 space-x-6 py-6 flex flex-row justify-between border border-slate-500">
         <li>
           <Link href="/">Home</Link>
         </li>
-        <li>Home</li>
-        <li>Home</li>
-        <li>Home</li>
+        <li>
+          <Link href="/about">About us</Link>
+        </li>
+        <li>
+          <Link href="/about-services">About Services</Link>
+        </li>
+        <li>
+          <Link href="/gallery">Gallery</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
       </ul>
     </nav>
   );
