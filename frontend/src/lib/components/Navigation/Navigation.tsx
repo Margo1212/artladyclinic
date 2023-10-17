ffe436d3ad519e7a427ada3411e3874ac16f8f43
Margo1212 <sirachenko944@gmail.com>
2023-10-17 22:53:59 +0200

    add navigation

--- frontend/src/lib/components/Navigation/Navigation.tsx
+++ frontend/src/lib/components/Navigation/Navigation.tsx
@@ -1,24 +1,63 @@
 import Link from "next/link";
+import Image from "next/image";
+import { Instagram } from "@svg/Instagram";
+import { Booksy } from "@svg/Booksy";
+import { Logo } from "@svg/Logo";
 
 const Navigation = () => {
   return (
-    <nav className="w-full flex justify-between">
-      <div>Artladyclinic</div>
-      <ul className="px-24 space-x-6 py-6 flex flex-row justify-between border border-slate-500">
+    <nav className="w-full flex justify-between items-center bg-[#f8f8f8] text-[#001965] px-14">
+      <div>
+        <Logo />
+      </div>
+      <ul className="px-24 space-x-8 align-middle py-6 flex flex-row justify-between">
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/about">O nas</Link>
         </li>
         <li>
-          <Link href="/about">About us</Link>
+          <Link href="/about-services">O usługach</Link>
         </li>
         <li>
-          <Link href="/about-services">About Services</Link>
+          <Link href="/gallery">Galeria</Link>
         </li>
         <li>
-          <Link href="/gallery">Gallery</Link>
+          <Link href="/price-list">Cennik</Link>
         </li>
         <li>
-          <Link href="/contact">Contact</Link>
+          <Link href="/products">Produkty</Link>
+        </li>
+        <li>
+          <Link href="/news">Nowośći</Link>
+        </li>
+        <li>
+          <Link href="/vouchers">
+            <p className="text-wrapper">Bony podarunkowe</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">Kontakt</Link>
+        </li>
+      </ul>
+      <ul className="flex space-x-2.5">
+        <li>
+          <Link
+            href={
+              "https://booksy.com/pl-pl/81163_art-lady_brwi-i-rzesy_8820_krakow#ba_s=sr_1"
+            }
+            target="_blank"
+          >
+            <Instagram />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={
+              "https://booksy.com/pl-pl/81163_art-lady_brwi-i-rzesy_8820_krakow#ba_s=sr_1"
+            }
+            target="_blank"
+          >
+            <Booksy />
+          </Link>
         </li>
       </ul>
     </nav>
