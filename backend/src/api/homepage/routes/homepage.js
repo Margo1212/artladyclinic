409d4230c249539f943216d8dac07af25bbb9e2b
Margo1212 <sirachenko944@gmail.com>
2023-10-18 12:19:05 +0200

    after update

--- backend/src/api/homepage/routes/homepage.js
+++ backend/src/api/homepage/routes/homepage.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * page router
+ * homepage router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::page.page');
+module.exports = createCoreRouter('api::homepage.homepage');
--- backend/src/api/homepage/services/homepage.js
+++ backend/src/api/homepage/services/homepage.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * page service
+ * homepage service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::page.page');
+module.exports = createCoreService('api::homepage.homepage');
