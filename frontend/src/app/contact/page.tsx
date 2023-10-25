5b5e36e9d1fd6b7ae76d0c19d88c4c872fceb223
Margo1212 <sirachenko944@gmail.com>
2023-10-25 15:58:56 +0200

    contact page

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import { ContactForm } from "@lib/components/ContactForm/ContactForm";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "...",
+  title: "Art Lady Clinic | Kontakt",
   description: "...",
 };
 
@@ -10,7 +10,18 @@ export default async function Page() {
   return (
     <div>
       <h2>Contact</h2>
-      <ContactForm />
+      <div className="grid grid-cols-2 grid-rows-6 gap-4">
+        <div className="row-span-3 w-full h-full">
+          <div className=""></div>
+          <div></div>
+        </div>
+        <div className="row-span-3 col-start-1 row-start-4 w-full h-full">
+          Mapa
+        </div>
+        <div className="row-span-6 col-start-2 row-start-1 w-full h-full">
+          <ContactForm />
+        </div>
+      </div>
     </div>
   );
 }
