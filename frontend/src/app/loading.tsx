6c68c6d3c0b67c833e2281c2a9f984d767d20aeb
Margo1212 <sirachenko944@gmail.com>
2023-11-05 14:29:33 +0100

    add loader

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -1,5 +1,12 @@
+"use client";
+import { InfinitySpin } from "react-loader-spinner";
+
 const Loading = () => {
-  return <div>blaloading...</div>;
+  return (
+    <div className="w-full flex justify-center">
+      <InfinitySpin width="200" color="#001965" />
+    </div>
+  );
 };
 
 export default Loading;
