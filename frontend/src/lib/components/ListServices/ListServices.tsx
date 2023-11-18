f68a99a4b02c86fc8ed0244280ce2b3d235e5b04
Margo1212 <sirachenko944@gmail.com>
2023-11-18 15:51:33 +0100

    add scrollbar

--- frontend/src/lib/components/ListServices/ListServices.tsx
+++ frontend/src/lib/components/ListServices/ListServices.tsx
@@ -4,7 +4,7 @@ import { Category, Service } from "types/types";
 import Image from "next/image";
 import { Tab } from "@headlessui/react";
 import Link from "next/link";
-import { Fragment, useState } from "react";
+import { ComponentPropsWithRef, Fragment } from "react";
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
@@ -12,7 +12,7 @@ function classNames(...classes: any) {
 
 export type CategoryList = Category[];
 
-type ListServicesProps = {
+type ListServicesProps = ComponentPropsWithRef<"div"> & {
   categories: Category[];
   services: Service[];
 };
@@ -23,15 +23,15 @@ export function ListServices(props: ListServicesProps) {
   if (!categories) return <p>No profile data</p>;
 
   return (
-    <div className="w-full h-full tablet:h-[33.438rem]">
+    <div className="w-full tablet:h-[33.438rem] bg-white">
       <Tab.Group vertical>
-        <div className="flex h-full tablet:flex-row flex-col w-full">
-          <Tab.List className="flex tablet:flex-col w-full flex-wrap tablet:flex-nowrap laptop:w-[280px] tablet:h-[33.438rem] tablet:space-y-1 rounded-sm text-dark-blue p-1 overflow-y-scroll">
+        <div className="grid grid-cols-4 h-full w-full">
+          <Tab.List className="flex col-span-1 flex-col laptop:w-[280px] tablet:h-[33.438rem] tablet:space-y-1 rounded-sm text-dark-blue p-2 overflow-auto scrollbar scrollbar-w-2 scrollbar-thumb-rounded-md scrollbar-track-white scrollbar-thumb-dark-blue">
             {categories.map((category) => (
               <Tab as={Fragment} key={category.id}>
                 <button
                   className={`
-                   group transition ease-in-out duration-200 gap-2 hover:bg-dark-blue w-full border-b-2 tablet:text-left border-dark-gray/20  tablet:justify-start grid  grid-cols-3 tablet:w-full outline-none h-14 rounded-sm px-4 py-2 text-sm font-normal leading-5`}
+                   group transition ease-in-out duration-200 hover:bg-dark-blue border-b-2 tablet:text-left border-dark-gray/20  tablet:justify-start grid  grid-cols-4 tablet:w-full outline-none h-14 rounded-sm px-2 py-2 text-sm font-normal leading-5`}
                 >
                   <span className="hidden col-span-1 w-full h-full group-hover:invert group-hover:brightness-0 tablet:flex  justify-center items-center ">
                     <Image
@@ -43,21 +43,21 @@ export function ListServices(props: ListServicesProps) {
                     />
                   </span>
 
-                  <p className="text-dark-blue col-span-2 group-hover:text-white  tablet:text-left font-medium text-[11px]">
+                  <p className="text-dark-blue h-full col-span-3 group-hover:text-white  tablet:text-left font-medium text-[11px]">
                     {category.name}
                   </p>
                 </button>
               </Tab>
             ))}
           </Tab.List>
-          <Tab.Panels className="h-full laptop:w-3/4 overflow-auto">
+          <Tab.Panels className="h-full w-full  col-span-3 overflow-auto scrollbar scrollbar-w-2 scrollbar-thumb-rounded-md scrollbar-track-white scrollbar-thumb-dark-blue">
             {categories.map((category) => (
               <Tab.Panel
                 key={category.id}
                 className={`${classNames(
                   "rounded-sm bg-white",
                   "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
-                )} p-6 h-full`}
+                )} py-6 px-7 h-full w-full`}
               >
                 <div className="flex flex-col items-center space-y-5">
                   <h2 className="text-base font-normal text-dark-blue">
@@ -67,7 +67,7 @@ export function ListServices(props: ListServicesProps) {
                     {category.description}
                   </p>
                 </div>
-                <ul className="flex flex-col">
+                <ul className="flex flex-col w-full ">
                   {services
                     .filter(
                       (service) => category.name === service.category?.name
@@ -89,7 +89,9 @@ export function ListServices(props: ListServicesProps) {
                               {service.name}
                             </h3>
                             <p className="text-[10px] font-light leading-5 text-[#565656]">
-                              {service.description}
+                              {service.description?.length >= 250
+                                ? service.description.substring(0, 250) + "..."
+                                : service.description}
                             </p>
                           </div>
                         </Link>
