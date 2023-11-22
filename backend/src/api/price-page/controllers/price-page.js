d7d174add9c375f23dbec5af522b56c40d0a35bd
Margo1212 <sirachenko944@gmail.com>
2023-11-22 15:38:02 +0100

    scemas changes

--- backend/src/api/price-page/controllers/price-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * price-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::price-page.price-page');
