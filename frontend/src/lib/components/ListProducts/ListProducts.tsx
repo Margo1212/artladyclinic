6658163c8cd908c0ef6fdec9fa557bad3d43c1ea
Margo1212 <sirachenko944@gmail.com>
2023-11-29 13:54:05 +0100

    fix products

--- frontend/src/lib/components/ListProducts/ListProducts.tsx
+++ frontend/src/lib/components/ListProducts/ListProducts.tsx
@@ -23,7 +23,7 @@ export function ListProducts(props: ListProductsProps) {
   if (!productCategories) return <p>No profile data</p>;
 
   return (
-    <div className="w-full min-h-[33.438rem] bg-white shadow-md pb-5  z-50">
+    <div className="w-full min-h-[33.438rem] bg-white shadow-md pb-5 mt-10 z-50">
       <Tab.Group vertical>
         <div className="flex flex-col laptop:flex-row bg-white w-full justify-around gap-5 laptop:gap-0">
           <Tab.List className="flex z-50  flex-col w-full laptop:w-1/4 h-full space-y-1 rounded-sm text-dark-blue   p-1">
