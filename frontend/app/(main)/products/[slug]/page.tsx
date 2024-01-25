693089094d42d62184dca181e345a81e379e2add
Margo1212 <marharytalesniak@gmail.com>
2024-01-25 14:47:55 +0100

    Refactor getProductBySlug function and remove unused code

--- frontend/app/(main)/products/[slug]/page.tsx
+++ frontend/app/(main)/products/[slug]/page.tsx
@@ -1,8 +1,30 @@
-import { getProductBySlug } from "@lib/data/products";
+import { request } from "@/lib/data";
+import { productsReducer } from "@/lib/utils";
 import { Product } from "@lib/types/types";
 import { ServiceDetailsImage } from "@ui/assets/svg/ServiceDetailsImage";
 import { Price } from "@ui/components/Price/Price";
 import Image from "next/image";
+import qs from "qs";
+
+export const getProductBySlug = async (slug: string) => {
+  const query = qs.stringify(
+    {
+      filters: {
+        slug: {
+          $eq: slug,
+        },
+      },
+      populate: ["product_category", "image"],
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+  const res = await request(`products?${query}`);
+
+  const rawProducts = res?.data[0];
+  return productsReducer(rawProducts);
+};
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const product: Product = await getProductBySlug(params.slug as string);
--- frontend/app/lib/data/products.ts
+++ frontend/app/lib/data/products.ts
@@ -1,6 +1,5 @@
 import { productsReducer } from "@lib/utils";
 import qs from "qs";
-import { cache } from "react";
 import { request } from "./index";
 
 export const getProducts = async () => {
@@ -22,27 +21,3 @@ export const getProducts = async () => {
   const products = rawProducts?.map((product: any) => productsReducer(product));
   return products;
 };
-
-export const preload = (slug: string) => {
-  void getProductBySlug(slug);
-};
-
-export const getProductBySlug = cache(async (slug: string) => {
-  const query = qs.stringify(
-    {
-      filters: {
-        slug: {
-          $eq: slug,
-        },
-      },
-      populate: ["product_category", "image"],
-    },
-    {
-      encodeValuesOnly: true,
-    }
-  );
-  const res = await request(`products?${query}`);
-
-  const rawProducts = res?.data[0];
-  return productsReducer(rawProducts);
-});
